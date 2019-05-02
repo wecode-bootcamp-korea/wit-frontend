063168141a857541722571300f17f5b790f3e9e8
mozart25 <mozartlee28@gmail.com>
2019-05-02 22:06:10 +0900

    second comiit y ban

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Login.scss';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <p> 안녕 난 로그인 페이지야! </p>
+    )
+  }
+}
+
+export default Login;
---
+++ src/Routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import Login from './Pages/Login/Login'
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </Router>
+    )
+  }
+}
+
+export default Routes;
