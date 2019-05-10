49cd6ccca02072c07e6873d1c9d377eb1e7becfc
mozart25 <mozartlee28@gmail.com>
2019-05-11 03:00:47 +0900

    운동타이머 추가세팅

--- src/Pages/StartInterval/StartInterval.js
+++ src/Pages/StartInterval/StartInterval.js
@@ -1,28 +1,60 @@
 import React from 'react';
 import './StartInterval.scss';
 
-const list = [{name: '달리기', action_time: 60, break_time: 10, set: 4},
+const user = [{name: '달리기', action_time: 310, break_time: 10, set: 4},
               {name: '스쿼트', action_time: 180, break_time: 20, set: 2},
               {name: '런지', action_time: 20, break_time: 10, set: 2},
               {name: '자전거돌리기', action_time: 60, break_time: 50, set: 5},
-              {name: '물구나무서기', action_time: 10, break_time: 60, set: 3},
-              {name: '팔굽혀펴기', action_time: 40, break_time: 10, set: 2},
-              {name: '사이드런지', action_time: 15, break_time: 20, set: 5},
-              {name: '스트레칭', action_time: 30, break_time: 24, set: 4},
-              {name: '넵넵넵', action_time: 45, break_time: 23, set: 3},
+              {name: '물구나무서기', action_time: 10, break_time: 60, set: 3}
             ]
+
+function getTotal(user) {
+  let total = 0;
+  for (var i=0; i < user.length; i++) {
+    total += (user[i].action_time * user[i].set) + user[i].break_time;
+  }
+  return total;
+}
+
+function getSetTotal(user) {
+  let set_total =0;
+  for (var i=0; i <user.length; i++) {
+    set_total += user[i].set;
+  }
+  return set_total;
+}
+
+function getTimeByEx(user) {
+  let temp=[];
+  let ex_tot=0;
+  for (var i=0; i <user.length; i++) {
+    ex_tot += user[i].action_time * user[i].set
+    temp.push(ex_tot);
+  }
+  return temp;
+}
+
+function getExName(user) {
+  let temp=[];
+  for (var i=0; i <user.length; i++) {
+    temp.push(user[i].name);
+  }
+  return temp;
+}
+
 
 class Interval extends React.Component {
   constructor() {
     super();
     this.state = {
-      list: list,
-      action_time: 320,
-      set: 3,
+      total_time: getTotal(user),
+      act_set_time: getTimeByEx(user)[0],
+      action_time: user[0].action_time,
+      total_set: getSetTotal(user),
+      current_ex: getExName(user)[0],
       clicked: false
     }
   }
-
 
 componentDidMount() {
   this.start()
@@ -32,7 +64,6 @@ componentWillUnmount() {
   clearInterval(this.intervalID)
 
 }
-
 
 start() {
   this.intervalID = setInterval( () => {
@@ -61,7 +92,6 @@ handleclick() {
   }
 }
 
-
   render() {
 
     return (
@@ -71,9 +101,18 @@ handleclick() {
         </div>
         <div className="back-ground">
           <div>
-            <p className="current-ex"> SQUAT </p>
-            <p className="time"> 6:00 </p>
-            <div className="clock">
+            <p className="current-ex"> {this.state.current_ex} </p>
+            <div className="toptimer">
+              <p className="time"> {Math.floor(this.state.act_set_time/60)}:</p>
+              <p className="time"> {this.state.act_set_time-Math.floor(this.state.act_set_time/60)*60} </p>
+            </div>
+            <div id="countdown">
+
+              <div className="clock">
+                <svg>
+                  <circle r="18" cx="31" cy="31"></circle>
+                </svg>
+              </div>
             </div>
             <p className="ticking">
                 {this.state.action_time}
@@ -86,7 +125,9 @@ handleclick() {
               </div>
             </div>
             <div>
-              <p className="totaltime"> 42:00</p>
+              <p className="totaltime"> {Math.floor(this.state.total_time/60)} :</p>
+              <p className="totaltime"> {this.state.total_time-Math.floor(this.state.total_time/60)*60} </p>
+              <p className="totalset"> / {this.state.total_set}</p>
             </div>
           </div>
         </div>
