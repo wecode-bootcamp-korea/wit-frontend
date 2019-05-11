3ba4a1e0facfeb0732bd96cf06b2d3a33d8803a3
mozart25 <mozartlee28@gmail.com>
2019-05-11 15:34:43 +0900

    conflict resolved

--- src/Components/Selectcircle/Selectcircle.js
+++ src/Components/Selectcircle/Selectcircle.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Selectcircle.scss';
+import { withRouter } from 'react-router-dom';
 
 class Selectcircle extends React.Component {
 
@@ -11,12 +12,18 @@ class Selectcircle extends React.Component {
   }
 }
 
+
+goToSettimer() {
+  this.props.history.push('/settimer');
+}
+
   render() {
     console.log(this.props)
 
     return (
       <div className={`blocking ${this.state.clicked ? 'active' : ''}`}
-            onClick={()=>{this.setState({clicked: !this.state.clicked })}}>
+            onClick={()=>{this.setState({clicked: !this.state.clicked })}}
+            onClick={this.goToSettimer.bind(this)}>
         <span className="dot"></span>
         <p className="ex-text">{this.props.info.name}</p>
       </div>
@@ -24,4 +31,4 @@ class Selectcircle extends React.Component {
   }
 }
 
-export default Selectcircle;
+export default withRouter(Selectcircle);
--- src/Pages/StartInterval/StartInterval.js
+++ src/Pages/StartInterval/StartInterval.js
@@ -2,29 +2,112 @@ import React from 'react';
 import './StartInterval.scss';
 
 
+const user = [{name: '달리기', action_time: 200, break_time: 10, set: 4},
+              {name: '스쿼트', action_time: 180, break_time: 20, set: 2},
+              {name: '런지', action_time: 20, break_time: 10, set: 2},
+              {name: '자전거돌리기', action_time: 60, break_time: 50, set: 5},
+              {name: '물구나무서기', action_time: 10, break_time: 60, set: 3}
+            ]
+
+function getTotal(user) {
+  let total = 0;
+  for (var i=0; i < user.length; i++) {
+    total += (user[i].action_time * user[i].set) + user[i].break_time;
+  }
+  return total;
+}
+
+function getSetTotal(user) {
+  let set_total =0;
+  for (var i=0; i <user.length; i++) {
+    set_total += user[i].set;
+  }
+  return set_total;
+}
+
+function getTimeByEx(user) {
+  let temp=[];
+  let ex_tot=0;
+  for (var i=0; i <user.length; i++) {
+    ex_tot += user[i].action_time * user[i].set
+    temp.push(ex_tot);
+  }
+  return temp;
+}
+
+function getExName(user) {
+  let temp=[];
+  for (var i=0; i <user.length; i++) {
+    temp.push(user[i].name);
+  }
+  return temp;
+}
+
+function getActualSet(user) {
+  let temp=[];
+  for (var i=0; i <user.length; i++) {
+    temp.push(user[i].set);
+  }
+  return temp;
+}
+
 class Interval extends React.Component {
   constructor() {
     super();
     this.state = {
-      action_choice: 180000,
-      break_choice : 10,
-      set_choice: 3,
+      total_time: getTotal(user),
+      act_set_time: getTimeByEx(user)[0],
+      action_time: user[0].action_time,
+      total_set: getSetTotal(user),
+      actual_set: getActualSet(user)[0],
+      current_ex: getExName(user)[0],
+      clicked: false
     }
   }
 
   componentDidMount() {
-    const intervalID = setInterval(() => {
+    this.start();
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalID)
+    clearInterval(this.totalID)
+    clearInterval(this.actsetID)
 
-      if (this.state.action_choice ===1) {
-        clearInterval(intervalID);
-      }
-      this.setState({
-        action_choice: Math.floor((this.state.action_choice % (1000 * 60)) / 1000)
-
-      })
-    }, 1000);
   }
 
+  start() {
+    this.intervalID = setInterval( () => {
+      if(this.state.action_time===1) {
+        this.stop()
+        //1세트 끝나는 즉시 우측 하단 1세트 증가
+        this.setState({
+          actual_set:this.state.actual_set + 1
+        })
+
+       }
+      this.setState({
+        action_time:this.state.action_time - 1
+      })
+    },10);
+
+    this.totalID = setInterval( () => {
+      this.setState({
+        total_time:this.state.total_time - 1
+      })
+    },1000);
+
+    this.actsetID = setInterval( () => {
+      this.setState({
+        act_set_time:this.state.act_set_time - 1
+      })
+    },1000);
+  }
+
+  stop() {
+    clearInterval(this.intervalID)
+    clearInterval(this.totalID)
+    clearInterval(this.actsetID)
+  }
 
   render() {
 
@@ -39,37 +122,20 @@ class Interval extends React.Component {
             <div id="status">
               <p id="aboutext"> 운동 진행 </p>
             </div>
-          </div>
-        </div>
-
-        <div className = "container">
-          <div id="first">
-            <p className="exname"> 팔굽혀 펴기</p>
-          </div>
-          <div id="second">
-            <p className="exname"> 윗몸일으켜기 </p>
-          </div>
-          <div id="third">
-            <p className="exname">스트레칭 </p>
-          </div>
-          <div id="fourth">
-            <p className="exname"> 도마뱀 </p>
-          </div>
-        </div>
-        <div>
-          <div className="timecontainer">
-           <p id="timer">
-               <span id="timer-mins"></span>
-               <span id="timer-secs"></span>
-               <span id="timer-msec"></span>
-           </p>
-          </div>
-        </div>
-
-        <div>
-          <div id="countdown">
-            <div id="countdown-number">
-              // <span id="timer-secs"></span>
+            <p className="ticking">
+                {this.state.action_time}
+            </p>
+              <div className={`kingofbutton ${this.state.clicked ? 'active' : ''}`}
+                onClick={this.handleclick.bind(this)}>
+              <div className="stopbutton">
+              </div>
+              <div className="stopbutton">
+              </div>
+            </div>
+            <div>
+              <p className="totaltime"> {Math.floor(this.state.total_time/60)} :</p>
+              <p className="totaltime"> {this.state.total_time-Math.floor(this.state.total_time/60)*60} </p>
+              <p className="totalset"> {this.state.actual_set} / {this.state.total_set} </p>
             </div>
               {this.state.action_choice}
 
